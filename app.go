5fa9874da50176b7baf364cd9c02ea5b564b38c5
Ethan Hicks <ehicks@berkeley.edu>
2025-08-09 20:28:50 -0400

    adding cors changes

--- app.go
+++ app.go
@@ -49,6 +49,21 @@ func main() {
 		_, _ = w.Write(content)
 	})
 
+	// Global CORS wrapper for all routes registered on DefaultServeMux
+	cors := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+			w.Header().Set("Access-Control-Max-Age", "86400")
+			if r.Method == http.MethodOptions {
+				w.WriteHeader(http.StatusNoContent)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+
 	log.Println("listening on", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+	log.Fatal(http.ListenAndServe(":"+port, cors(http.DefaultServeMux)))
 }
